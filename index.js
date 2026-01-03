afc544722923e8d7ed7baf229918c25f395f8e08
Pedro <eupedrobarbosa03@gmail.com>
2026-01-03 17:00:28 -0300

    reconstruindo aplicação

--- index.js
+++ index.js
@@ -42,129 +42,91 @@ class Student {
         this.secondBimester = secondBimester
         this.thirdBimester = thirdBimester
         this.fourthBimester = fourthBimester
-        this.validationFinished = false;
     };
 
     validation() {
 
-        let numbers = [];
-
-        for (let i = 0; i < 10; i++) {
-            i = String(i);
-            numbers.push(i);
-        };
-
-        for (let i = 0; i < this.name.length; i++) {
-            if (numbers.includes(this.name[i])) {
-                boxError(inputsSelection.studentName, "box-error", "add");
-                return;
-            };
-        }
-
-        if (!this.name || typeof this.name !== "string" || this.name.trim() === "") {
-            boxError(inputsSelection.studentName, "box-error", "add");
-            return;  
-        };
-
-        const lengthMax = 20;
-
-        if (this.name.length > lengthMax) {
-            boxError(inputsSelection.studentName, "box-error", "add");
-            return;  
-        }
-
         this.name = this.name.trim();
+        const regExpStudentName = /[a-zA-Z]+/gi;
+        const studentNameLength = {min: 3, max: 40}
+
+        if (!this.name.match(regExpStudentName)) {
+            boxError(inputsSelection.studentName, "box-error", "add"); return;
+        }
+
+        if (this.name.length < studentNameLength.min
+            || this.name.length > studentNameLength.max) {
+            boxError(inputsSelection.studentName, "box-error", "add"); return;  
+        };
 
         boxError(inputsSelection.studentName, "box-error", "remove");
 
-        this.firstBimester = Number(this.firstBimester);
-        this.secondBimester = Number(this.secondBimester);
-        this.thirdBimester = Number(this.thirdBimester);
-        this.fourthBimester = Number(this.fourthBimester);
+        const bimesters = [
+            {bimester: this.firstBimester, input: inputsSelection.firstBimester},
+            {bimester: this.secondBimester, input: inputsSelection.secondBimester},
+            {bimester: this.thirdBimester, input: inputsSelection.thirdBimester},
+            {bimester: this.fourthBimester, input: inputsSelection.fourthBimester}
+        ];
 
-        function validationBimester(bimester, input) {
+        let isReal = true;
 
-            if (bimester < 0 || bimester > 10 || Number.isNaN(bimester)) {
-                boxError(input, "box-error", "add");
-                return false;
-            };
+        bimesters.forEach((student) => {
+            if (Number(student.bimester) < 0
+            || Number(student.bimester) > 10
+            || isNaN(student.bimester)
+            || !student.bimester){
+                boxError(student.input, "box-error", "add")
+                isReal = false;
+            }
+        });
 
-            boxError(input, "box-error", "remove");
-            return true;
+        if (!isReal) return;
 
+        bimesters.forEach((bimester) => bimester.input.classList.remove("box-error"));
+
+        const notes = bimesters.reduce((note, sumNote) => {
+            return note + Number(sumNote.bimester);
+        }, 0);
+
+        const avarage = parseFloat((notes / 4).toFixed(1));
+        let situation = null;
+
+        if (avarage >= 5) situation = "approved"
+        else if (avarage >= 4.5 && avarage <= 4.9) situation = "recovery"
+        else situation = "reproved";
+
+        const li = document.createElement("li");
+        li.innerHTML = 
+        `
+            <div>
+                <p id="first-result"><span>Name - </span> ${this.name}</p>
+                <p id="second-result"><span>Final Note - </span> ${notes}</p>
+                <p id="third-result"><span>Average - </span> ${avarage}</p>
+            </div>
+            <div>
+                <p id="fourth-result"><span>Situation - </span>${situation}</p>
+            </div>
+        `
+
+        switch(situation) {
+            case 'approved':
+                list.appendChild(li);
+                li.classList.add("approved");
+                break;
+            case 'recovery':
+                list.appendChild(li);
+                li.classList.add("recovery");
+                break;
+            case 'reproved':
+                list.appendChild(li);
+                li.classList.add("reproved");
+                break;
+            default: console.error(`...`);
         };
 
-        if (!validationBimester(this.firstBimester, inputsSelection.firstBimester)) return;
-        if (!validationBimester(this.secondBimester, inputsSelection.secondBimester)) return;
-        if (!validationBimester(this.thirdBimester, inputsSelection.thirdBimester)) return;
-        if (!validationBimester(this.fourthBimester, inputsSelection.fourthBimester)) return;
-        
-
-        this.validationFinished = true;
+        cleanInputs();
 
     };
-
-    result() {
-
-        if (this.validationFinished) {
-
-            let notaSum = 0;
-            let notes = [this.firstBimester, this.secondBimester, this.thirdBimester, this.fourthBimester];
-            for (let i = 0; i < notes.length; i++) {
-                notaSum += notes[i];
-            };
-
-            let average = (notaSum / notes.length).toFixed(1);
-            
-            let situation = "";
-
-            if (average >= 5) {
-                situation = "approved";
-            } else if (average >= 4.5 && average <= 4.9) {
-                situation = "recovery";
-            } else {
-                situation = "reproved";
-            };
-
-            const template =  
-            `
-                <li>
-                    <div>
-                        <p id="first-result"><span>Name - </span> ${this.name}</p>
-                        <p id="second-result"><span>Final Note - </span> ${notaSum.toFixed(1)}</p>
-                        <p id="third-result"><span>Average - </span> ${average}</p>
-                    </div>
-
-                    <div>
-                        <p id="fourth-result"><span>Situation - </span>${situation}</p>
-                    </div>
-                </li>
-            `
-
-            const parser = new DOMParser();
-            const templateHtml = parser.parseFromString(template, "text/html");
-            const li = templateHtml.querySelector("li");
-
-            switch(situation) {
-                case 'approved':
-                    list.appendChild(li);
-                    li.classList.add("approved");
-                    break;
-                case 'recovery':
-                    list.appendChild(li);
-                    li.classList.add("recovery");
-                    break;
-                case 'reproved':
-                    list.appendChild(li);
-                    li.classList.add("reproved");
-                    break;
-            };
-
-            cleanInputs();
-
-        };
-    };
-
 };
 
 buttons.send.addEventListener("click", () => {
@@ -172,7 +134,6 @@ buttons.send.addEventListener("click", () => {
 
     const student = new Student(studentName.value, firstBimester.value, secondBimester.value, thirdBimester.value, fourthBimester.value);
     student.validation();
-    student.result();
 });
 
 buttons.clean.addEventListener("click", () => {
